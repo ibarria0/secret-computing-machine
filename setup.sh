a00900ad66ba4a14347c85b53f355619b331500c
Baconator507 <ibarria0@gmail.com>
2013-12-09 18:46:39 -0500

    http and libboost

--- setup.sh
+++ setup.sh
@@ -11,9 +11,9 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 #hay que instalar un pocoton de vainas para poder hacer los builds
-sudo apt-get install build-essential libssl-dev libboost-all-dev libboost-chrono1.48-dev libboost-filesystem1.48-dev libboost-system1.48-dev libboost-program-options1.48-dev libboost-thread1.48-dev libminiupnpc-dev libdb4.8-dev libdb4.8++-dev git -y
+sudo apt-get install build-essential libssl-dev libboost-dev boost-defaults libminiupnpc-dev libdb4.8-dev libdb4.8++-dev git -y
 
-git clone git@github.com:donSchoe/xolominer.git
+git clone https://github.com/donSchoe/xolominer.git
 cd xolominer
 cd src
 make -f makefile.unix
