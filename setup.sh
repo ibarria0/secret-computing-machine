b8322d711e6af83784acdc536ed1defadaf4cfb0
Baconator507 <ibarria0@gmail.com>
2013-12-09 22:57:04 -0500

    space

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get remove --purge libreoffice-* rhythmbox
+sudo apt-get remove --purge libreoffice-* rhythmbox thunderbird* shotwell* 
 
 #bitcoin ppa
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
@@ -9,12 +9,13 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 #hay que instalar un pocoton de vainas para poder hacer los builds
-sudo apt-get install build-essential libssl-dev boost-defaults libminiupnpc-dev libdb4.8-dev libdb4.8++-dev git -y
+sudo apt-get install build-essential libssl-dev libminiupnpc-dev libdb++-dev libdb-dev git -y
 
 #build libboost
 cd
 wget -O boost.tar.gz 'http://downloads.sourceforge.net/project/boost/boost/1.48.0/boost_1_48_0.tar.gz'
 tar -xvf boost.tar.gz
+rm -rf boost.tar.gz
 cd boost_1_48_0
 sudo ./bootstrap.sh
 sudo ./bjam install
