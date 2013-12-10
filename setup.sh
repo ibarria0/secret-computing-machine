fb862b6b56d53e0411bc15c54feddcb9995e42d7
Baconator507 <ibarria0@gmail.com>
2013-12-10 17:18:07 -0500

    use 4.8

--- setup.sh
+++ setup.sh
@@ -12,6 +12,10 @@ sudo apt-get upgrade -y
 #hay que instalar un pocoton de vainas para poder hacer los builds
 sudo apt-get install build-essential libssl-dev libminiupnpc-dev libdb++-dev libdb-dev git gcc-4.8 g++-4.8 libgcc-4.8-dev libboost-all-dev libboost-dev libgmp3-dev -y
 
+#latest gcc and g++
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+
 cd
 git clone https://github.com/donSchoe/xolominer.git
 cd xolominer
