cfdff87f6f407aa6a4b524ef0985a1ee8a843bac
Baconator507 <ibarria0@gmail.com>
2013-12-21 20:48:06 -0500

    updated setup for 13.10

--- setup.sh
+++ setup.sh
@@ -2,22 +2,14 @@
 #add this line if you want to get some free space
 #sudo apt-get remove --purge libreoffice-* rhythmbox thunderbird* shotwell* 
 
-#bitcoin ppa
-sudo add-apt-repository ppa:bitcoin/bitcoin -y
-
 #updates
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
 #hay que instalar un pocoton de vainas para poder hacer los builds
-sudo apt-get install build-essential libssl-dev libminiupnpc-dev libdb++-dev libdb-dev git gcc-4.8 g++-4.8 libgcc-4.8-dev libboost-all-dev libboost-dev libgmp3-dev -y
-
-#latest gcc and g++
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+sudo apt-get install build-essential libssl-dev libminiupnpc-dev libdb++-dev libdb-dev git g++ libgcc-4.8-dev libboost1.54-all-dev libgmp3-dev -y
 
 cd
-git clone https://github.com/donSchoe/xolominer.git
-cd xolominer
-cd src
+git clone https://github.com/thbaumbach/primecoin.git
+cd xolominer/src
 make -f makefile.unix
